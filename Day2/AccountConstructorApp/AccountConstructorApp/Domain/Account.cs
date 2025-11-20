7bd320b819294a5d88dad1c66cbc40fa0eadd26d
kannan <kannans89@hotmail.com>
2025-11-20 19:22:04 +0530

    account

---
+++ Day2/AccountConstructorApp/AccountConstructorApp/Domain/Account.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountConstructorApp.Domain
+{
+    class Account
+    {
+        //fields
+        private readonly string _accno; //immutable
+        private readonly string _name; //immutable
+        private  double _balance;//mutable
+
+        private const int MinBalance = 500;
+        //constructor
+
+        
+        public Account(string accno, string name, double balance)
+        {
+
+            _accno = accno;
+            _name = name;
+            _balance = balance;
+        }
+        public Account(string accno, string name)
+        {
+            _accno = accno;
+            _name = name;
+            _balance = MinBalance;
+        }
+
+        //methods
+        public void Deposit(double amount) {
+
+            _balance += amount;
+        }
+        public void Withdraw(double amount) {
+            if (_balance - amount > MinBalance)
+               _balance = _balance - amount;
+            else
+                Console.WriteLine("Error to be thrown");
+        }
+
+        //properties
+        public string Accno { 
+          get { return _accno; } //readonly property
+        }
+        public string Name {
+            get {
+                return _name;
+            }
+           
+        }
+
+        public double Balance {
+            get {
+                return _balance;
+            }
+        }
+
+    }
+}
--- Day2/AccountConstructorApp/AccountConstructorApp/Program.cs
+++ Day2/AccountConstructorApp/AccountConstructorApp/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace AccountConstructorApp
+﻿using AccountConstructorApp.Domain;
+using System;
+
+namespace AccountConstructorApp
 {
-    internal class Program
+     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            Account accountofAlex = new Account("1001", "Alexander", 1000);
+            Account accountofDaniel = new Account("2001", "Daniel");
+
+            PrintInfo(accountofAlex);
+            accountofAlex.Deposit(500);//Deposit(accountofalex,500);
+            PrintInfo(accountofAlex);
+
+            PrintInfo(accountofDaniel);
+            accountofDaniel.Withdraw(100);
+            PrintInfo(accountofDaniel);
+
+           // accountofAlex.Name = "ALeX";
+
+        }
+
+        static void PrintInfo(Account account)
+        {
+
+            Console.WriteLine($"Name is {account.Name}");
+            Console.WriteLine($"Accno is {account.Accno}");
+            Console.WriteLine($"Balance is {account.Balance}");
+            Console.WriteLine();
         }
     }
 }
