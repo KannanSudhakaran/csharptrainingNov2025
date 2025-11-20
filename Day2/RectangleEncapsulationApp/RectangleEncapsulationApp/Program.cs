6d2bd05df7d09cd65911db9b980b226b8b2909c4
kannan <kannans89@hotmail.com>
2025-11-20 16:55:36 +0530

    array of rectnagle

--- Day2/RectangleEncapsulationApp/RectangleEncapsulationApp/Program.cs
+++ Day2/RectangleEncapsulationApp/RectangleEncapsulationApp/Program.cs
@@ -7,7 +7,51 @@ namespace RectangleEncapsulationApp
     {
         static void Main()
         {
+            // CaseStudy1();
 
+            CaseStudy2();
+
+        }
+
+        private static void CaseStudy2()
+        {
+            Rectangle[] manyRectangles = new Rectangle[3];
+            manyRectangles[0] = new Rectangle();
+            manyRectangles[1] = new Rectangle();
+            manyRectangles[2] = new Rectangle();
+
+            manyRectangles[0].SetWidth(10);
+            manyRectangles[0].SetHeight(20);
+            manyRectangles[0].SetColor("red");
+
+            manyRectangles[1].SetWidth(20);
+            manyRectangles[1].SetHeight(30);
+            manyRectangles[1].SetColor("blue");
+
+            manyRectangles[2].SetWidth(50);
+            manyRectangles[2].SetHeight(60);
+            manyRectangles[2].SetColor("green");
+
+
+            PrintInfo(manyRectangles);
+
+
+        }
+
+        private static void PrintInfo(Rectangle[] rectangles)
+        {
+            foreach (Rectangle rectangle in rectangles) {
+
+                Console.WriteLine($"width is {rectangle.GetWidth()}");
+                Console.WriteLine($"height is {rectangle.GetHeight()}");
+                Console.WriteLine($"coor is {rectangle.GetColor()}");
+                Console.WriteLine();
+            }
+            
+        }
+
+        private static void CaseStudy1()
+        {
             Rectangle smallRectangle = new Rectangle();
             smallRectangle.SetWidth(100);
             Console.WriteLine($"width is {smallRectangle.GetWidth()}");
@@ -17,7 +61,6 @@ namespace RectangleEncapsulationApp
 
             smallRectangle.SetColor("pink");
             Console.WriteLine(smallRectangle.GetColor());
-            
         }
     }
 }
