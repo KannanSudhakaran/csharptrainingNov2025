a79c7a1586dbe29bad0cfdcd26800db80a168345
kannan <kannans89@gmail.com>
2025-11-27 14:49:33 +0530

    day3 this pointer

---
+++ Day3/PlayerThisPointerApp/PlayerThisPointerApp/Domain/Player.cs
@@ -0,0 +1,53 @@
+﻿
+using System;
+
+namespace PlayerThisPointerApp.Domain
+{
+    class Player
+    {
+        //attributes
+        //constructor
+        //methods
+        //properties
+
+        private readonly string _name;
+        private readonly int _age;
+        private const int MIN_AGE = 20;
+
+        public Player(string _name, int _age) {
+
+           this._name = _name;
+           this._age = _age;
+
+            Console.WriteLine("Player created with 2params");
+        }
+
+        public Player(string _name) : this(_name,MIN_AGE)
+        {
+
+            Console.WriteLine("player created with 1prams");
+        }
+
+
+        public Player WhoIsElder(Player secondPlayer) {
+
+            if (this._age > secondPlayer._age)
+            {
+                return this;
+            }
+
+
+            return secondPlayer;
+        }
+
+        public string Name
+        {
+            get { return _name; } 
+        }
+
+        public int Age { 
+          get  { return _age; }
+        }
+
+    }
+}
---
+++ Day3/PlayerThisPointerApp/PlayerThisPointerApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using PlayerThisPointerApp.Domain;
+using System;
+
+namespace PlayerThisPointerApp
+{
+     class Program
+    {
+        static void Main()
+        {
+            Player alexander = new Player("Alexander",25);
+            PrintDetails(alexander);
+
+
+            Player daniel = new Player("Daniel");
+            PrintDetails(daniel);
+
+           Player elder = alexander.WhoIsElder(daniel);//WhoIsElder(alexander,deniel);
+           PrintDetails(elder);
+        }
+
+        private static void PrintDetails(Player player)
+        {
+            Console.WriteLine($"name is {player.Name}, age is {player.Age}");
+            Console.WriteLine($"hashcode {player.GetHashCode()}");
+        }
+    }
+}
