b666b01805b1c7abdeb192b64ca1e0b03590eedd
kannan <kannans89@hotmail.com>
2025-12-04 14:36:51 +0530

    hashset

---
+++ Day5/CollectionsAndGenericsApp/CollectionsAndGenericsApp/Domain/OrderItem.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionsAndGenericsApp.Domain
+{
+    class OrderItem
+    {
+        //fields are generated automatically
+       
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Quanity { get; set; } 
+        public double UnitPrice { get; set; }
+
+        public double TotalCost() { 
+          return Quanity* UnitPrice; 
+        }
+
+        //callback
+        public override int GetHashCode()
+        {
+            return this.Id;
+        }
+
+        //callabck
+        public override bool Equals(object? second)
+        {
+          var secondOrderItem=   second as OrderItem;
+
+            return (secondOrderItem.Id == this.Id
+                  && secondOrderItem.Name == this.Name);
+
+
+            
+
+        }
+
+    }
+}
---
+++ Day5/CollectionsAndGenericsApp/CollectionsAndGenericsApp/Program.cs
@@ -0,0 +1,189 @@
+﻿
+using CollectionsAndGenericsApp.Domain;
+using System.Collections;
+
+namespace CollectionsAndGenericsApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //CaseStudy1();
+
+            // CaseStudy2();
+
+            //CaseStudy3();
+
+            CaseStudy4();
+        }
+
+        private static void CaseStudy4()
+        {
+
+            var setOfUniqueOrderItems = new HashSet<OrderItem>();
+
+            var orderItem1 = new OrderItem
+            {
+                UnitPrice = 5,
+                Name = "Milk",
+                Quanity = 2,
+                Id = 1
+            };
+
+            var orderItem2 = new OrderItem
+            {
+                Id = 2,
+                Name = "Biscuits",
+                Quanity = 10,
+                UnitPrice = 1
+
+            };
+
+            var orderItem3 = new OrderItem
+            {
+                Id = 2,
+                Name = "Biscuits",
+                Quanity = 10,
+                UnitPrice = 1
+
+            };
+
+            //Console.WriteLine(orderItem1.GetHashCode());
+            //Console.WriteLine(orderItem2.GetHashCode());
+            //Console.WriteLine(orderItem3.GetHashCode());
+
+            setOfUniqueOrderItems.Add(orderItem1);
+            setOfUniqueOrderItems.Add(orderItem2);
+            setOfUniqueOrderItems.Add(orderItem3);
+
+            Console.WriteLine($"{orderItem2.GetHashCode()==orderItem3.GetHashCode()}");
+            //Console.WriteLine(orderItem3==orderItem2);
+            Console.WriteLine($"{orderItem2.Equals(orderItem3)}");
+
+            Console.WriteLine(setOfUniqueOrderItems.Count);
+          
+
+
+        }
+
+        private static void CaseStudy3()
+        {
+
+            var setOfUniqueNames = new HashSet<string>();
+            setOfUniqueNames.Add("Daniel");
+            setOfUniqueNames.Add("Elena");
+            setOfUniqueNames.Add("Alexander");
+            setOfUniqueNames.Add("Daniel");
+
+            Console.WriteLine(setOfUniqueNames.Count);
+
+            foreach (var name in setOfUniqueNames)
+            {
+                Console.WriteLine(name);
+            }
+
+        }
+
+        private static void CaseStudy2()
+        {
+            //generics collections are type safe
+          
+            
+            var orderItem1 = new OrderItem
+            {
+                UnitPrice = 5,
+                Name = "Milk",
+                Quanity = 2,
+                Id = 1
+            };
+
+            var orderItem2 = new OrderItem
+            {
+                Id = 2,
+                Name = "Biscuits",
+                Quanity = 10,
+                UnitPrice = 1
+
+            };
+
+            var orderItem3 = new OrderItem
+            {
+                Id = 3,
+                Name = "Butter",
+                Quanity = 10,
+                UnitPrice = 2
+            };
+
+            List<OrderItem> orderBasket = new List<OrderItem>();
+            orderBasket.Add(orderItem1);
+            orderBasket.Add(orderItem2);
+            orderBasket.Add(orderItem3);
+           // orderBasket.Add("Daniel");//compile time error
+            //orderBasket.Add(100);//compile time error
+
+            double checkoutPrice = 0;
+            foreach (OrderItem item in orderBasket)
+            {
+
+                Console.WriteLine(item.Name);
+                Console.WriteLine(item.UnitPrice + " " + item.Quanity);
+                checkoutPrice = checkoutPrice + item.TotalCost();
+            }
+
+            Console.WriteLine("Checkout price at counter :" + checkoutPrice);
+
+        }
+
+        private static void CaseStudy1()
+        {
+            //collections are not type safe
+
+            var item1 = new OrderItem {
+             UnitPrice=5,
+             Name="Milk",
+             Quanity=2,
+             Id=1
+            };
+
+            var item2 = new OrderItem {
+             Id=2,
+             Name="Biscuits",
+             Quanity=10,
+             UnitPrice=1
+
+            };
+
+            var item3 = new OrderItem { 
+             Id=3,
+             Name= "Butter",
+             Quanity=10,
+             UnitPrice=2
+            };
+
+            ArrayList orderBasket = new ArrayList();
+            orderBasket.Add(item1);
+            orderBasket.Add(item2);
+            orderBasket.Add(item3);
+           // orderBasket.Add("Daniel");
+           orderBasket.Add(100);
+
+            double checkoutPrice = 0;
+            foreach (object box in orderBasket) {
+
+               OrderItem item= (OrderItem) box;
+
+                Console.WriteLine(item.Name);
+                Console.WriteLine(item.UnitPrice+ " "+ item.Quanity);
+                checkoutPrice =checkoutPrice+ item.TotalCost();
+
+
+            }
+
+            Console.WriteLine("Checkout price at counter :"+checkoutPrice);
+            
+            
+        }
+    }
+}
